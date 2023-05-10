c6bbdb207bd300f59f00836634458c30747f9849
Lily Zeiset <lilyzeiset@gmail.com>
2023-05-10 18:47:32 -0400

    started skeleton component structure

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+/**
+ * Homepage for the site
+ */
+export default function Home() {
+  return (
+    <Typography variant="h4">
+      Click search to get started!
+    </Typography>
+  )
+}
---
+++ src/components/Search.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+
+/**
+ * Search page
+ */
+export default function Search() {
+  return (
+    <Typography variant="h4">
+      This is the search page. 
+      It will have a form to search for reservations 
+      and send user to SearchResults page
+    </Typography>
+  )
+}
