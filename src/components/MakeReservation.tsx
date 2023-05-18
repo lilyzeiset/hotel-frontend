1512ba57173028afef049a3042230e3b7c024098
Lily Zeiset <lilyzeiset@gmail.com>
2023-05-18 11:10:43 -0400

    cancel reservation handler

--- src/components/MakeReservation.tsx
+++ src/components/MakeReservation.tsx
@@ -35,7 +35,8 @@ export default function MakeReservation() {
 
   function handleCancelReservation() {
     //delete reservtion
-    navigate('/home');
+    clearTimeout(timeoutRef);
+    navigate('/');
   }
 
   return (
