22a412c54cbbe5c2cf32133c9f95c456a37111dc
Lily Zeiset <lilyzeiset@gmail.com>
2023-05-22 11:41:40 -0400

    unused var

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from "react-i18next";
  */
 export default function Home() {
 
-  const {t, i18n} = useTranslation();
+  const {t} = useTranslation();
 
   return (
     <Typography variant="h4">
